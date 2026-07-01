560d4cbc995cd1111b6afaf98af0e7c564464746
Shinon16 <cparamo2d@gmail.com>
2026-07-01 15:11:30 -0600

    Formato de salida de tokens en tabla modificacion 3 de tabla

--- Competencia2/Ejercicio3/test.py
+++ Competencia2/Ejercicio3/test.py
@@ -1,33 +1,25 @@
-# Importa ANTLR4 para funciones
-from antlr4 import*
+from antlr4 import *
 from ExprLexer import ExprLexer
 import sys
 
-
-# Lo que obtiene es la entrada , analiza el texto y lo separa en tokes
+# Obtiene la entrada y la separa en tokens
 input_stream = FileStream(sys.argv[1])
 
-
 lexer = ExprLexer(input_stream)
-# Toma los tokens que produjo el lexer y los guarda en un flujo/lista
+
+# Guarda los tokens generados por el lexer
 tokens = CommonTokenStream(lexer)
 tokens.fill()
-print(tokens)
 
-
-print(f"{'LEXEMA':<15} {'TOKEN':<15} {'TIPO':<6} {'LINEA':<6} {'COLUMNA':<8}")
+print(f"{'LEXEMA':<15} {'TOKEN':<15} {'TIPO':<6} {'LINEA':<8} {'COLUMNA':<8}")
 print("-" * 60)
 
 for token in tokens.tokens:
-    print("Texto ", token.text)
-    print("Linea ", token.line)
-    print("Columna ", token.column)
+
+    # Ignorar EOF
     if token.type == Token.EOF:
         continue
 
     nombre_token = lexer.symbolicNames[token.type]
-    print("Tipo ", nombre_token)
-    
-    print("-------------------")
 
-    print(f"{token.text:<15} {nombre_token:<15} {token.type:<6} {token.line:<6} {token.column:<8}")
+    print(f"{token.text:<15} {nombre_token:<15} {token.type:<6} {token.line:<8} {token.column:<8}")
